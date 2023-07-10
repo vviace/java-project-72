d2b8308da8cb77e77b1c8883884996a6d834c23c
Vasilev Viacheslav <vviace@yandex.ru>
2023-07-10 09:12:52 +0300

    update build

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,27 +1,22 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
-import hexlet.code.domain.query.QUrl;
 import hexlet.code.domain.UrlCheck;
+import hexlet.code.domain.query.QUrl;
 import io.ebean.PagedList;
-
 import io.javalin.http.Handler;
-
-import java.net.URL;
-import java.util.List;
-
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.HttpResponse;
-
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class UrlController {
